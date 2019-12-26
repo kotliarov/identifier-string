faf16e531ad2284278c18cc9d730b17a2362fab2
Alexander Kotliarov <alexander.kotliarov@gmail.com>
2019-12-25 19:30:15 -0500

    Added implementations of chains and polymers of the SPL document model

--- identifier_string.py
+++ identifier_string.py
@@ -102,41 +102,101 @@ from collections import defaultdict
 
 from lxml import etree
 
-NAMESPACES = {"x": "urn:hl7-org:v3"}
 
-class Chains(object):
-    """ SPL document protein chains.
-    """
-    xpath_moiety = "/x:moiety[x:code[@code=\"C118424\"]"
-    xpath_local_id = "./x:partMoiety/x:id/@extension"
-    xpath_aa = "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@media-type=\"application/x-aa-seq\"]"
 
-    def __init__(self, doc):
-        """ 
+class SplDocument(object):
+    NAMESPACES = {"x": "urn:hl7-org:v3"}
+    SPLDescriptor = {
+        "document" : {
+            "xpath"       : "/x:document[x:code[@code = '64124-1']]",
+            "uri"         : "document/code[code=64124-1]",
+            "content"     : ["section"],
+            "mandatory"   : True,
+            "cardinality" : 1
+        },
+        "section" : {
+            "xpath"    : "./x:component/x:structuredBody/x:component/x:section[x:code[@code='48779-3']]",
+            "uri"      : "section/code[code=48779-3]",
+            "content"  : ["substance-main", "substance-other"],
+            "mandatory": True,
+            "cardinality" : 1
+        },
+        "substance-main" : {
+            "xpath"      : "./x:subject/x:identifiedSubstance/x:identifiedSubstance[x:code[@codeSystem = '2.16.840.1.113883.4.9']]",
+            "mandatory": True,
+            "cardinality": 1
+        },
+        "substance-other" : {
+            "xpath"      : "./x:subject/x:identifiedSubstance/x:identifiedSubstance[x:code[@codeSystem != '2.16.840.1.113883.4.9']]",
+            "mandatory" : False
+        },
+    }
+    
+    def __init__(self, filepath):
+        with open(docpath, 'r') as src:
+            doc = src.read()
+        self.dom = etree.fromstring(doc)
+        self.document_ = None
+        self.section_ = None
+        self.substance_ = None
+        self.substance_other_ = None
+
+    def document(self):
+        """ Return document element.
         """
-        self._counter = 0
-        self._chains = []
-        self._load(doc)
+        if self.document_ is None:
+            desc = self.SPLDescriptor["document"]
+            nodes = self.dom.xpath(desc["xpath"], namespaces=self.NAMESPACES)
+            if len(nodes) != 1:
+                raise SPLDocumentError("Document element must be present and unique")
+            self.document_ = nodes[0]
+        return self.document_
 
-
-    def _load(self, doc):
-        """ Load chains defined in the SPL XML document.
-        :doc: SPL XML DOM object
+    def section(self):
+        """ Return secrion element
         """
-        nodes = doc.xpath(self.xpath_moiety, namesoaces=NAMESPACES)
-        for node in nodes:
-            local_id = node.xpath(self.xpath_localid, namespaces=NAMESPACES)
-            if not local_id:
-                raise SPLDocumentError("local id not found")
+        if self.section_ is None:
+            desc = self.SPLDescriptor["section"]
+            nodes = self.document().xpath(desc["xpath"], namespaces=self.NAMESPACES)
+            if len(nodes) != 1:
+                raise SPLDocumentError("Section element must be present and unique")
+            self.section_ = nodes[0]
+        return self.section_
 
-            sequence = node.xpath(self.xpath_aa, namespaces=NAMESPACES)
-            if not sequence:
-                raise SPLDocumentError("Polypeptide chain AA sequence not found")
+    def substance(self):
+        """ Return main substance element.
+        """
+        if self.substance_ is None:
+            desc = self.SPLDescriptor["substance-main"]
+            nodes = self.section().xpath(desc["xpath"], namespaces=self.NAMESPACES)
+            if len(nodes) != 1:
+                raise SPLDocumentError("Main substance element must be present and unique")
+            self.substance_ = nodes[0]
+        return self.substance_
+
+    def substance_other(self):
+        if self.substance_other_ is None:
+            desc = self.SPLDescriptor["substance-other"]
+            nodes = self.section().xpath(desc["xpath"], namespaces=self.NAMESPACES)
+            self.substance_other_ = nodes
+        return self.substance_other_
+
+    def select(self, base, query):
+        """
+        """
+        return base.xpath(query, namespaces=self.NAMESPACES)
 
 
-class ProteinSplModel(object):
+class SPLDocumentError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class SplModelProtein(object):
     def __init__(self, xmldoc):
-        pass
+        self.chains = Chains(xmldoc)
+        self.polymers = Polymers(xmldoc)
+        self.modifications = []
 
     def accept(self, visitor):
         """
@@ -145,6 +205,259 @@ class ProteinSplModel(object):
         visitor.visit("chains", self.chains)
         visitor.visit("polymers", self.polymers)
         visitor.visit("modifications", self.modifications)
+
+
+class Chains(object):
+    """ SPL document protein chains.
+    """
+    xpath_moiety = "./x:moiety[x:code[@code=\"C118424\"]]"
+    xpath_localid = "./x:partMoiety/x:id/@extension"
+    xpath_aa = "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@mediaType=\"application/x-aa-seq\"]/text()"
+
+    def __init__(self, doc):
+        """ 
+        """
+        self._counter = 0
+        self._pos = 0
+        self.chains = []
+        self._load(doc)
+
+    def _load(self, doc):
+        """ Load chains defined in the SPL XML document.
+        :doc: SPL XML DOM object
+        """
+        substance = doc.substance()
+        nodes = substance.xpath(self.xpath_moiety, namespaces=doc.NAMESPACES)
+        for node in nodes:
+            local_id = node.xpath(self.xpath_localid, namespaces=doc.NAMESPACES)
+            if not local_id:
+                raise SPLDocumentError("local id not found")
+
+            value = node.xpath(self.xpath_aa, namespaces=doc.NAMESPACES)
+            if not value:
+                raise SPLDocumentError("Polypeptide chain AA sequence not found")
+            self.chains.append(Chain(local_id[0], value[0]))
+        self.chains = sorted(self.chains, key=lambda x: (x.value, x.local_id))
+        for index, chain in enumerate(self.chains):
+            chain.name = "chain{}".format(self._counter)
+            self._counter += 1
+        self._lookup = {x.local_id: x for x in self.chains}
+
+    def __getitem__(self, key):
+        """ Return chain descriptor by local id
+        """
+        return self._lookup[key]
+
+    def __iter__(self):
+        self._pos = 0
+        return self
+
+    def __next__(self):
+        if self._pos < len(self.chains):
+            i = self._pos
+            self._pos += 1
+            return self.chains[i]
+        else:
+            raise StopIteration()
+
+class Chain(object):
+    def __init__(self, local_id, value):
+        self.local_id = local_id
+        self.value = value
+        self.name = None
+
+
+class Polymers(object):
+    """ SPL document polymers / irregular AA.
+    """
+    xpath_moiety = "./x:moiety[x:code[@code=\"C118424\"]]"
+    xpath_localid = "./x:code/@code"
+
+    def __init__(self, doc):
+        """ 
+        """
+        self._counter = 0
+        self._pos = 0
+        self.polymers = []
+        self._load(doc)
+
+
+    def _load(self, doc):
+        """ Load polymers / irrgegula AA moleculs defined in the SPL XML document (other substance(s)).
+        :doc: SPL XML DOM object
+        """
+       
+        def read_code(subject):
+            """ Reurn aux substance code"""
+            code = subject.xpath(self.xpath_localid, namespaces=doc.NAMESPACES)
+            if len(code) != 1:
+                raise SPLDocumentError("Aux substance code not found")
+            return code[0]
+        
+        def get_moiety(subject):
+            """ Return moiety that representis subject's chemichal structure.
+            """
+            moiety_code = sub.xpath("./x:asSpecializedKind/x:generalizedMaterialKind/x:code/@code", namespaces=doc.NAMESPACES)
+            if len(moiety_code) != 1:
+                raise SPLDocumentError("Moiety code not found")
+            moiety = subject.xpath("./x:moiety[x:partMoiety/x:code[@code=\"{}\"]]".format(moiety_code[0]),
+                               namespaces=doc.NAMESPACES)
+            if len(moiety) != 1:
+                raise SPLDocumentError("Moiety \"{}\" not found".format(moiety_code[0]))
+            return moiety[0]
+    
+        def get_connection_points(subject):
+            """
+            """
+            class ConnectionPoint(object):
+                def __init__(self, amino_group, carboxyl_group):
+                    self.amino_group = amino_group
+                    self.carboxyl_group = carboxyl_group
+
+                def to_string(self):
+                    return "N{}C{}".format(self.amino_group, self.carboxyl_group)
+            
+            points = []
+            nodes = subject.xpath("./x:moiety[x:code[@code=\"C118427\"]]", namespaces=doc.NAMESPACES)
+            for node in nodes:
+                positions = node.xpath("./x:positionNumber/@value", namespaces=doc.NAMESPACES)
+                points.append(ConnectionPoint(positions[0], positions[1]))
+            return points
+
+
+        subjects = doc.substance_other()
+        if subjects is not None:
+            for sub in subjects:
+                code = read_code(sub)
+                moiety = get_moiety(sub)
+                conn_points = get_connection_points(sub)
+                value = get_chem_structure(moiety, None)
+                quantity = get_quantity(moiety)
+                self.polymers.append(Polymer(code, value, conn_points, quantity))
+        self.polymers = sorted(self.polymers, key=lambda x: (x.value, x.code))
+        self._lookup = {x.code: x for x in self.polymers}
+        for index, x in enumerate(self.polymers):
+            x.name = "poly{}".format(self._counter)
+            self._counter += 1
+
+
+    def __getitem__(self, key):
+        """ Return chain descriptor by local id
+        """
+        return self._lookup[key]
+
+    def __iter__(self):
+        self._pos = 0
+        return self
+
+    def __next__(self):
+        if self._pos < len(self.polymers):
+            i = self._pos
+            self._pos += 1
+            return self.polymers[i]
+        else:
+            raise StopIteration()
+
+
+class Polymer(object):
+    def __init__(self, code, value, conn_points, quantity):
+        self.code = code
+        self.value = value
+        self.conn_points = conn_points
+        self.quantity = quantity
+        self.name = None
+    
+    @property
+    def connection_points(self):
+        """ Return connection points
+        """
+        return ",".join([x.to_string() for x in self.conn_points])
+
+
+CHEMICAL_STRUCT = [
+    ("x-inchi-key", "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@mediaType=\"application/x-inchi-key\"]/text()"),
+    ("x-inchi", "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@mediaType=\"application/x-inchi\"]/text()"),
+    ("x-mdl-molfile", "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@mediaType=\"application/x-mdl-molfile\"]/text()"),
+    ("x-aa=seq", "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@mediaType=\"application/x-aa-seq\"]/text()"),
+    ("x-na-seq", "./x:subjectOf/x:characteristic[x:code[@code=\"C103240\"]]/x:value[@mediaType=\"application/x-na-seq\"]/text()"),
+]
+
+def get_chem_structure(moiety, mediaType, namespaces=SplDocument.NAMESPACES):
+    """ Return chemocal struct balue.
+        :moiety: xml dom element 
+        :mediaType: concrete mediaType or None if any
+    """
+    def get_value(query):
+        nodes = moiety.xpath(query, namespaces=namespaces)
+        if nodes:
+            return nodes[0]
+        else:
+            return None
+
+    for media, query in CHEMICAL_STRUCT:
+        if mediaType is None or mediaType == media:
+            value = get_value(query)
+            if value is not None or mediaType is not None:
+                return value
+    return None
+
+def get_quantity(moiety, namespaces=SplDocument.NAMESPACES):
+    """
+    """
+    class Quantity(object):
+        def __init__(self, num, denom, unit):
+            self.num = num
+            self.denom = denom
+            self.unit = unit
+
+        def to_string(self):
+            return "{}:{}:{}".format(self.num, self.denom. self.unit)
+
+    class QuanityRange(object):
+        def __init__(self, low, low_inclusive, high, high_inclusive,  denom, unit):
+            self.low = low
+            self.low_incl = low_inclusive
+            self.high = high
+            self.high_incl = high_inclusive
+            self.denom = denom
+            self.unit = unit
+
+        def to_string(self):
+            return "{}{},{}{}:{}:{}".format("[" if self.low_incl else "(",
+                                            self.low,
+                                            self.high,
+                                            "]" if self.high_incl else ")",
+                                            self.denom. 
+                                            self.unit)
+
+
+    def is_inclusive(el):
+        rc = True
+        if "inclusive" in el:
+            if "false" == el["inclusive"]:
+                rc = False
+        return rc
+
+    numerator = moiety.xpath("./x:quantity/x:numerator", namespaces=namespaces)[0]
+    denominator = moiety.xpath("./x:quantity/x:denominator", namespaces=namespaces)[0]
+    
+    attributes = denominator.attrib
+    unit = attributes["unit"]
+    denom_value = attributes["value"]
+    
+    attributes = numerator.attrib
+    if "value" in attributes:
+        num_value = attributes["value"]
+        return Quantity(num_value, denom_value, unit)
+    else: # range
+        low = numerator.xapth("./x:low")[0]
+        high = numerator.xapth("./x:high")[0]
+        return QuantityRange(low.attrib["value"],
+                             is_inclusive(low),
+                             high.attrib["value"],
+                             is_inclusive(high),
+                             denom_value,
+                             unit)
 
 
 class IdentifierStringTemplate(object):
@@ -398,12 +711,6 @@ class StringTemplate(object):
         return gen.to_string(self._context)
 
 
-
-class SPLDocumentError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-
 def make_dom(docpath):
     """
     REturn XML DOM object.
@@ -434,8 +741,8 @@ Rules = {
         "str" : "{{ name }}:{{ value }}"
     },
     "polymer" : {
-        "attributes" : ["name", "value"],
-        "str" : "{{ name }}:{{ value }}"
+        "attributes" : ["name", "value", "connection_points"],
+        "str" : "{{ name }}:{{ value }}:{{ connection_points }}"
     },
     "modification" : {
         "attributes" : ["name", "chain", "position", "type"],
@@ -475,12 +782,11 @@ class MockPoly(object):
 
 if __name__ == '__main__':
     docpath = sys.argv[1]
-    spl_doc = make_dom(docpath)
     identifier = IdentifierStringTemplate(Templates(Rules))
-    ##model = make_document_model(spl_doc)
-    ##model.accept(identifer)
-    mock_model = MockModel()
-    mock_model.accept(identifier)
+    model = SplModelProtein(SplDocument(docpath))
+    model.accept(identifier)
+    #mock_model = MockModel()
+    #mock_model.accept(identifier)
     print(identifier.to_string(Rules["protein_identifier"]["str"]))
     
 
