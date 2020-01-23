ae3bcf972a453c32fde2140bb3bef3ed642d866b
Alexander Kotliarov <alexander.kotliarov@gmail.com>
2020-01-22 22:52:14 -0500

    Specify encoding when opening a file. On Windows it would add BOM when no encoding is specified.

--- src/idstring/spl.py
+++ src/idstring/spl.py
@@ -33,7 +33,7 @@ class SplDocument(object):
     }
 
     def __init__(self, file_path):
-        with open(file_path, 'r') as src:
+        with open(file_path, 'r', encoding='utf-8') as src:
             doc = src.read()
         self.dom = etree.fromstring(doc)
         self.document_ = None
